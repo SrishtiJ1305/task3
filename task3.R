b9649ebe65bb42ee1320d5300ba9eb92be6b73d6
SrishtiJ1305 <59254025+SrishtiJ1305@users.noreply.github.com>
2020-01-04 10:37:34 +0100

    Add files via upload

---
+++ task3.R
@@ -0,0 +1,18 @@
+library(lattice)
+
+#create a simple data set with 4 different variable (W,X,Y,Z) where W,X,Y are numeric and Z is character with different group character (a, b ,c)
+
+data_lattice1 = data.frame(W=c(49,27,32,50),X=c(45,14,15,17), Y=c(52,23,15,10), Z=c("a", "b", "a", "b"))
+data_lattice2 = data.frame(W=c(40,27,32,50),X=c(45,15,15,17), Y=c(51,26,16,10), Z=c("a", "b", "a", "b"))
+
+#show your Data set
+View(data_lattice1)
+View(data_lattice2)
+
+
+#plot a scatter plot -make a plot from data shown above in two different panels, split by variable z -Make two data series on the same plot.
+
+cloud(W~X*Y|Z, data_lattice1)
+cloud(W~X*Y|Z, data_lattice2)
+
+
